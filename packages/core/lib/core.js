13e88193b5ae1288faddbb0a6664b87216e748ce
lyleXX <740334078@qq.com>
2024-04-25 21:47:22 +0800

    init

---
+++ packages/core/lib/core.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const utils = require('@lyle-cli-dev/utils')
+
+module.exports = core
+
+function core() {
+  return 'Hello from core'
+}
+
+utils()
---
+++ packages/utils/lib/utils.js
@@ -0,0 +1,8 @@
+'use strict'
+
+module.exports = utils
+
+function utils() {
+  console.log('2')
+  return 'Hello from utils'
+}
