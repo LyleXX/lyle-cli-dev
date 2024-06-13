d29f3036b9c33b8e6e600fc41bf6a73c95c1ccb5
LyleXX <740334078@qq.com>
2024-06-14 07:42:00 +0800

    项目模版开发

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -7,6 +7,8 @@ const inquirer = require('inquirer');
 const fse = require('fs-extra');
 const semver = require('semver');
 
+const getProjectTemplate = require('./getProjectTemplate');
+
 const TYPE_PROJECT = 'project';
 const TYPE_COMPONENT = 'component';
 
@@ -21,9 +23,12 @@ class InitCommand extends Command{
     async exec(){
         try{
             // 1. 准备阶段
-            const ret =  await this.prepare()
-            if(ret){
+            const projectInfo =  await this.prepare()
+            if(projectInfo){
                 // 2. 下载模板
+                log.verbose('projectInfo',projectInfo)
+                this.projectInfo = projectInfo
+                this.downloadTemplate()
                 // 3. 安装模板
             }
         }
@@ -33,7 +38,21 @@ class InitCommand extends Command{
 
     }
 
+    downloadTemplate(){
+        // 1. 通过项目模板API获取项目模板信息
+        // 1.1 通过egg.js搭建一套后端系统
+        // 1.2 通过npm存储项目模板(vue-cli/vue-element-admin)
+        // 1.3 将项目模板信息存储到mongodb数据库中
+        // 1.4 通过egg.js获取mongodb中的数据并且通过API返回
+    }
+
    async prepare(){
+        // 0. 判断项目模板是否存在
+       const template = await getProjectTemplate();
+      if(!template || template.length === 0){
+          throw new Error('项目模板不存在')
+      }
+      this.template = template
        const localPath = process.cwd();
      // 1. 判断当前目录是否为空
         if(!this.isDirEmpty(localPath)){
@@ -90,7 +109,7 @@ class InitCommand extends Command{
        log.verbose('type',type);
        if(type === TYPE_PROJECT){
            // 2. 获取项目基本信息
-           const o = await inquirer.prompt([{
+           const project = await inquirer.prompt([{
                type:'input',
                name:'projectName',
                message:'请输入项目名称',
@@ -137,8 +156,16 @@ class InitCommand extends Command{
                        return v
                    }
                }
+           },{
+               type:'list',
+               name:'projectTemplate',
+               message:'请选择项目模板',
+               choices:this.createTemplateChoice()
            }])
-           console.log(o)
+           projectInfo = {
+                type,
+                ...project
+           }
        }else{
 
        }
@@ -153,6 +180,13 @@ class InitCommand extends Command{
             !file.startsWith('.') && ['node_modules'].indexOf(file) < 0
         ))
         return !fileList || fileList.length <= 0
+    }
+
+    createTemplateChoice(){
+        return this.template.map(item =>({
+            value:item.npmName,
+            name:item.name
+        }))
     }
 }
 
---
+++ utils/request/README.md
@@ -0,0 +1,11 @@
+# `@lyle-cli-dev/request`
+
+> TODO: description
+
+## Usage
+
+```
+const request = require('@lyle-cli-dev/request');
+
+// TODO: DEMONSTRATE API
+```
